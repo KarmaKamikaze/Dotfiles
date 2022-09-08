f490a9ebf33a129717d51b056a4e28a8c7f7f750
KarmaKamikaze <nicolaijgn@gmail.com>
2022-09-08 11:24:58 +0200

    Update fish.config to support exa & other aliases

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,9 +1,31 @@
 # ~/.config/fish/config.fish
-set fish_greeting
+
+### ADDING TO THE PATH
+# First line removes the path; second line sets it.  Without the first line,
+# your path gets massive and fish becomes very slow.
+set -e fish_user_paths
+set -U fish_user_paths $HOME/.local/bin $HOME/Applications $fish_user_paths
+
+### EXPORT ###
+set fish_greeting                                 # Supresses fish's intro message
+set EDITOR "nvim"                                 # $EDITOR use neovim in terminal
 
 ### RANDOM COLOR SCRIPT ###
 colorscript random
 
+### SET MANPAGER
+### Uncomment only one of these!
+
+### "bat" as manpager
+# set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
+
+### "vim" as manpager
+# set -x MANPAGER '/bin/bash -c "vim -MRn -c \"set buftype=nofile showtabline=0 ft=man ts=8 nomod nolist norelativenumber nonu noma\" -c \"normal L\" -c \"nmap q :qa<CR>\"</dev/tty <(col -b)"'
+
+### "nvim" as manpager
+set -x MANPAGER "nvim -c 'set ft=man' -"
+
+### FUNCTIONS ###
 
 # Functions needed for !! and !$
 function __history_previous_command
@@ -33,5 +55,32 @@ else
   bind '$' __history_previous_command_arguments
 end
 
+### ALIASES ###
 
+# navigation
+alias ..='cd ..'
+alias ...='cd ../..'
+alias .3='cd ../../..'
+alias .4='cd ../../../..'
+alias .5='cd ../../../../..'
+
+# Changing "ls" to "exa"
+alias ls='exa -al --color=always --group-directories-first' # my preferred listing
+alias la='exa -a --color=always --group-directories-first'  # all files and dirs
+alias ll='exa -l --color=always --group-directories-first'  # long format
+alias lt='exa -aT --color=always --group-directories-first' # tree listing
+alias l.='exa -a | egrep "^\."'
+
+# Colorize grep output (good for log files)
+alias grep='grep --color=auto'
+alias egrep='egrep --color=auto'
+alias fgrep='fgrep --color=auto'
+
+# confirm before overwriting something
+alias cp="cp -i"
+alias mv='mv -i'
+alias rm='rm -i'
+
+
+### SETTING THE STARSHIP PROMPT ###
 starship init fish | source
